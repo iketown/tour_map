d03d7be318f733babc7e9274f6ec1ba4b4e0bd25
iketown <be@ike.works>
2022-02-09 17:51:10 -0600

    map box

---
+++ pages/api/amadeus.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+//@ts-ignore
+import Amadeus from "amadeus";
+
+const amadeus = new Amadeus({
+  clientId: process.env.AMADEUS_CLIENT_ID,
+  clientSecret: process.env.AMADEUS_CLIENT_SECRET,
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  return amadeus.shopping.flightOffersSearch
+    .get({
+      originLocationCode: "BOS",
+      destinationLocationCode: "MSP",
+      departureDate: "2022-07-01",
+      adults: "1",
+    })
+    .then(function (data) {
+      console.log(JSON.stringify(data));
+      return res.status(200).json(data);
+    })
+    .catch(function (responseError) {
+      console.log(JSON.stringify(responseError));
+      return res.status(400).json(responseError);
+    });
+};
