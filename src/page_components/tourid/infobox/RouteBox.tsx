ac1cc68c582eef1b01a4c5ae2c475a367cef274e
iketown <be@ike.works>
2022-02-17 07:58:22 -0600

    map machine

--- src/page_components/tourid/infobox/RouteBox.tsx
+++ src/page_components/tourid/infobox/RouteBox.tsx
@@ -3,12 +3,15 @@ import { useMapboxCtx } from "~/contexts/MapboxCtx";
 import DataView from "~/components/DataView";
 import { isEqual } from "lodash";
 import { useTourCtx } from "~/contexts/TourCtx";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { DoubleArrow } from "@mui/icons-material";
+import { NextLinkComposed } from "~/components/Link";
+import { useMapDirections } from "~/hooks/useMapDirections";
 
 const RouteBox = () => {
   const { mapService } = useMapboxCtx();
-  const { tourInfo } = useTourCtx();
+  const { tourInfo, tour_id } = useTourCtx();
+  const { getDirections } = useMapDirections();
   const { fromEventId, toEventId } = useSelector(
     mapService,
     ({ context }) => {
@@ -22,7 +25,7 @@ const RouteBox = () => {
   let summary = (
     <Box sx={{ display: "flex", alignItems: "center" }}>
       <Typography component="span" variant="h5">
-        {fromEvent?.city || "?"}
+        {fromEvent?.city || ""}
       </Typography>
       <Box sx={{ mx: 1, display: "flex", alignItems: "center" }}>
         <DoubleArrow color="disabled" />
@@ -33,11 +36,46 @@ const RouteBox = () => {
     </Box>
   );
 
+  const fromText = fromEvent ? (
+    <Typography component="span">
+      from <b>{fromEvent.city}</b>
+    </Typography>
+  ) : (
+    ""
+  );
+  const toText = toEvent ? (
+    <Typography component="span">
+      to <b>{toEvent.city}</b>
+    </Typography>
+  ) : (
+    ""
+  );
+
+  const getDir = () => {
+    if (!fromEvent || !toEvent) return;
+    getDirections([fromEvent.loc, toEvent.loc]);
+  };
+
   return (
     <div>
       {summary}
-      <DataView data={{ fromEventId, toEventId }} title="from to ids" />
-      <DataView data={tourInfo} title="tourInfo" />
+      <Box>
+        {fromEvent && toEvent && (
+          <Button onClick={getDir}>driving directions</Button>
+        )}
+      </Box>
+      {tour_id && (
+        <NextLinkComposed
+          href="/admin/tours/[tour_id]/travel"
+          to={`/admin/tours/${tour_id}/travel?o=${fromEventId}&d=${toEventId}`}
+        >
+          <Typography>
+            Edit travel {fromText} {toText}{" "}
+          </Typography>
+        </NextLinkComposed>
+      )}
+      {/* <DataView data={{ fromEventId, toEventId }} title="from to ids" /> */}
+      {/* <DataView data={tourInfo} title="tourInfo" /> */}
     </div>
   );
 };
